938441d1c7752d6a5eea498a0d8bb96bd66295f1
Weatherly02 <47132416+Weatherly02@users.noreply.github.com>
2019-05-03 17:27:24 -0500

    junit test homework

--- src/test/java/edu/dmacc/codedsm/GameLogicTest.java
+++ src/test/java/edu/dmacc/codedsm/GameLogicTest.java
@@ -1,5 +1,7 @@
+
 package edu.dmacc.codedsm;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,5 +11,61 @@ public class GameLogicTest {
 
     @Before
     public void setUp() throws Exception {
+    }
+
+    @Test
+    public void isVictoriousHasScoreOf10000() {
+        StubGameRunner stubGameRunner = new StubGameRunner() {
+            @Override
+            public boolean isVictorious() {
+                return true;
+            }
+        };       //new gamerunner
+        GameLogic gameLogic = new GameLogic(stubGameRunner);  //new gamelogic
+        GameResult gameResult = gameLogic.runGame("Tam" +
+                "my");  //call the method that you want to test
+        assertEquals(10000, gameResult.getScore().intValue()); //assert what you are testing score
+    }
+
+    @Test
+    public void buildResult() {
+
+    }
+
+    @Test
+    public void isDefeatedHasScoreOf100() {
+        StubGameRunner stubGameRunner = new StubGameRunner() {
+            @Override
+            public boolean isDefeated() {
+                return true;
+            }
+        };       //new gamerunner
+        GameLogic gameLogic = new GameLogic(stubGameRunner);  //new gamelogic
+        GameResult gameResult = gameLogic.runGame("Paige");  //call the method that you want to test
+        assertEquals(100, gameResult.getScore().intValue()); //assert what you are testing score
+    }
+
+    @Test
+    public void playerName() {
+        StubGameRunner stubGameRunner = new StubGameRunner() {
+            @Override
+            public boolean isDefeated() {
+                return true;
+            }
+        };       //new gamerunner
+        GameLogic gameLogic = new GameLogic(stubGameRunner);  //new gamelogic
+        GameResult gameResult = gameLogic.runGame("Kelly");  //call the method that you want to test
+        assertEquals("Kelly", gameResult.getPlayer()); //assert what you are testing score
+    }
+
+    private class StubGameRunner implements GameRunner {   //had to create since we did not have class that implements
+
+        public boolean isVictorious() {
+            return false;
+        }
+
+        public boolean isDefeated() {
+            return false;
+        }
     }
 }
